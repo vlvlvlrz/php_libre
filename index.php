b8d12e64cefa0725f8da373baf17cf3f2cbd591c
oktestit <vladislavismepower@gmail.com>
2019-10-23 21:23:57 +0300

    basic
    Basic

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>YEAHBOIZ</title>
+  </head>
+  <body>
+    <?php
+      print "HEELOW";
+     ?>
+
+   <form method="POST" action="sayhello.php">
+     Your Name: <input type="text" name = "user"/>
+     <br/>
+      Your Age <input type="number" name = "digit"/>
+     <br/>
+     <button type="submit"> Say Hello</button>
+   </form>
+
+  </body>
+</html>
---
+++ sayhello.php
@@ -0,0 +1,8 @@
+<?php
+  print "Hello , ";
+  print $_POST['user'];
+  print "!";
+  //код просто так, для примера
+  print " And your age is ";
+  print $_POST['digit'];
+ ?>
